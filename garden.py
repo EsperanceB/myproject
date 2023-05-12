8a38eef9aa705941daf8b3357d25525d5a20b7df
Esperance <129316382+EsperanceB@users.noreply.github.com>
2023-05-12 22:18:31 +0100

    Update garden.py

--- garden.py
+++ garden.py
@@ -10,9 +10,7 @@ gardenpathSentences = ["The horse raced past the barn fell.",
                        "Mary gave the child a Band-Aid","That Jill is never here hurts",
                        "The cotton clothing is made of grows in Mississippi"]
 
-# Iterate through the list to extracte strings from the list, you would get the 
-# same result using one string instead I did this for the requirements of the task I believed.
-# After running, we realise that spaCy doesn't provide named entity recognition for most of the Garden path sentences   
+# Iterate through the list to extracte strings from the lists   
 for sentence in gardenpathSentences:
     nlp_sentence = nlp(sentence)
     print([token.orth_ for token in nlp(sentence)])    # Tokenise the sentences
@@ -22,9 +20,5 @@ for sentence in gardenpathSentences:
 print(spacy.explain("GPE"))
 print(spacy.explain("ORG"))
 
-# GPE: The explaination for GPE is Countries, cities, states
-# ORG: The explaination for ORG is Companies, agencies, institutions, etc.
-# The first definition by spacy is clear to understand and is what it's supposed be.
-# While for the Band-Aid definition, SpaCy understand it's an institution because of the Aid suffix 
-# thus this was about medical bandage.
+
 
